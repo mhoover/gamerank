95db9d7be3acf3c7c44cfa020720f0a9c29f2939
Matt Hoover <matthew.a.hoover@gmail.com>
2016-02-21 19:48:33 -0500

    Fix up tests.

--- parser.py
+++ parser.py
@@ -7,31 +7,31 @@ import numpy as np
 from xml.etree import ElementTree
 from collections import defaultdict
 
-url = 'http://www.boardgamegeek.com/xmlapi2'
+URL = 'http://www.boardgamegeek.com/xmlapi2'
 
 def get_hotness_data(type):
-    """ Use BoardGameGeek API to get hotness rank for specified type. 
+    """ Use BoardGameGeek API to get hotness rank for specified type.
     Returns an XML object. """
-    d = requests.get('{}/hot?type={}'.format(url, type))
+    d = requests.get('{}/hot?type={}'.format(URL, type))
     return ElementTree.fromstring(d.content)
 
 
 def get_thing_data(type, ids):
     """ Use BoardGameGeek API to get game statistics. Takes in ids of
-    interest and parameter to identify if statistics are wanted and 
-    returns an XML object with requested game information and 
+    interest and parameter to identify if statistics are wanted and
+    returns an XML object with requested game information and
     statistics.
 
-    type = boardgame, but could be something else supported by 
-    BoardGameGeek, like boardgameexpansion, videogame, rpgitem, or 
+    type = boardgame, but could be something else supported by
+    BoardGameGeek, like boardgameexpansion, videogame, rpgitem, or
     rpgissue
-    ids = individual id or list of ids; needs to be in the form 
-    '111' or '111, 112' -- can do this by wrapping a list in str() 
-    stats = default to True; provides additional metrics that are 
+    ids = individual id or list of ids; needs to be in the form
+    '111' or '111, 112' -- can do this by wrapping a list in str()
+    stats = default to True; provides additional metrics that are
     usually desired """
-    ids = re.sub('^0\s{1,}|\n[\w\s:,]{1,}', '', 
+    ids = re.sub('^0\s{1,}|\n[\w\s:,]{1,}', '',
                  re.sub('\n[0-9]{1,}\s{1,}', ', ', ids))
-    d = requests.get('{}/thing?type={}&id={}&stats=1'.format(url, type, 
+    d = requests.get('{}/thing?type={}&id={}&stats=1'.format(URL, type,
                     ids))
     return ElementTree.fromstring(d.content)
 
@@ -54,7 +54,7 @@ def xml_to_df(xml, type='hotness'):
         df = _construct_thing_df(d)
     else:
         raise Exception('Invalid \'type\'; try again.')
-    
+
     df['date'] = time.strftime('%d-%b-%Y')
     return df
 
@@ -63,7 +63,7 @@ def _construct_thing_df(d):
     df = pd.DataFrame()
     df['id'] = d['items_item_id']
     df['type'] = d['items_item_type']
-    df['name'] = ([name for name, conditional in zip(d['item_name_value'], 
+    df['name'] = ([name for name, conditional in zip(d['item_name_value'],
                   d['item_name_type']) if conditional=='primary'])
     df['published'] = d['item_yearpublished_value']
     df['playing_time'] = d['item_playingtime_value']
@@ -71,13 +71,13 @@ def _construct_thing_df(d):
     df['avg_score'] = d['ratings_average_value']
     df['bayes_score'] = d['ratings_bayesaverage_value']
     df['sd'] = d['ratings_stddev_value']
-    df['category'] = ([name for name, conditional in 
-                      zip(d['ranks_rank_name'], d['ranks_rank_type']) if 
+    df['category'] = ([name for name, conditional in
+                      zip(d['ranks_rank_name'], d['ranks_rank_type']) if
                       conditional=='family'])
-    df['category_rank'] = ([name for name, conditional in 
-                           zip(d['ranks_rank_value'], d['ranks_rank_type']) if 
+    df['category_rank'] = ([name for name, conditional in
+                           zip(d['ranks_rank_value'], d['ranks_rank_type']) if
                            conditional=='family'])
-    df['category_avg'] = ([name for name, conditional in 
-                          zip(d['ranks_rank_bayesaverage'], 
+    df['category_avg'] = ([name for name, conditional in
+                          zip(d['ranks_rank_bayesaverage'],
                           d['ranks_rank_type']) if conditional=='family'])
     return df
--- tests/parser_tests.py
+++ tests/parser_tests.py
@@ -4,7 +4,7 @@ from xml.etree import ElementTree
 from gamerank.parser import *
 
 
-@pytest.mark.parametrize(('data', 'exp1', 'exp2', 'exp3'), [
+@pytest.mark.parametrize('data, exp1, exp2, exp3', [
     ('boardgame', 'tag', 'attrib', 'makeelement')
 ])
 def test_get_hotness_data(data, exp1, exp2, exp3):
@@ -16,10 +16,9 @@ def test_get_hotness_data(data, exp1, exp2, exp3):
     assert hasattr(test_case, exp3)
 
 
-@pytest.mark.parametrize(('data_type', 'data_ids', 'data_stats', 'exp1', 
-                         'exp2', 'exp3'), [
-    ('boardgame', '38821', True, 'tag', 'attrib', 'makeelement'), 
-    ('boardgame', '38821, 2651', True, 'tag', 'attrib', 'makeelement'), 
+@pytest.mark.parametrize('data_type, data_ids, data_stats, exp1, exp2, exp3', [
+    ('boardgame', '38821', True, 'tag', 'attrib', 'makeelement'),
+    ('boardgame', '38821, 2651', True, 'tag', 'attrib', 'makeelement'),
     ('boardgame', '38821, 2651', False, 'tag', 'attrib', 'makeelement')
 ])
 def test_get_thing_data(data_type, data_ids, data_stats, exp1, exp2, exp3):
@@ -31,19 +30,18 @@ def test_get_thing_data(data_type, data_ids, data_stats, exp1, exp2, exp3):
     assert hasattr(test_case, exp3)
 
 
-@pytest.mark.parametrize(('test', 'exp_id', 'exp_name', 'exp_votes', 
-                         'exp_votes2', 'exp_bayes'), [
-    (ElementTree.parse('ex1.xml').getroot(), '175914', 'Food Chain Magnate', 
-     '0', '1', '7'), 
-    (ElementTree.parse('ex2.xml').getroot(), '999999', 'Food Chain Magnate2', 
-     '10', '12', '6.99568'), 
-    (ElementTree.parse('ex3.xml').getroot(), '1422', 'Food Chain Magnate3', 
-     '2', '10', '6.09568') 
+@pytest.mark.parametrize(('test, exp_id, exp_name, exp_votes, exp_votes2, exp_bayes', [
+    (ElementTree.parse('ex1.xml').getroot(), '175914', 'Food Chain Magnate',
+     '0', '1', '7'),
+    (ElementTree.parse('ex2.xml').getroot(), '999999', 'Food Chain Magnate2',
+     '10', '12', '6.99568'),
+    (ElementTree.parse('ex3.xml').getroot(), '1422', 'Food Chain Magnate3',
+     '2', '10', '6.09568')
 ])
-def test_loop_through_xml_tree(test, exp_id, exp_name, exp_votes, exp_votes2, 
+def test_loop_through_xml_tree(test, exp_id, exp_name, exp_votes, exp_votes2,
                                exp_bayes):
     test_case = loop_through_xml_tree(test, defaultdict(list))
-    
+
     assert test_case['item_id'] == [exp_id]
     assert test_case['name_value'] == [exp_name]
     assert test_case['result_numvotes'] == [exp_votes]
